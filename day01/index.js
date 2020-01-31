7c0047060031aa8e01048cba286e2de7b9538a9c
Luis Hernandez <luis@heysparkbox.com>
2020-01-31 16:12:30 -0500

    feat: Add day01 part 2 integration.
    Adding solution to part 2 of day01. getFuelForFuelWeight works in isolation. However, the preprocessing of input data should abrstacted from the final compososed funciton getTotalFuel.
    
    Updated name of previous getTotalFuel to reduceFuelArr as it is a better semantic representation of the funciton.

--- day01/index.js
+++ day01/index.js
@@ -4,25 +4,30 @@ const path = require('path');
 
 const inputData = fs.readFileSync(path.resolve(__dirname, 'data.txt'), 'utf8');
 
-console.log(inputData);
-
 const convertInputToArr = input =>
   input.split(/\n/).map(item => parseInt(item, 10));
 
-console.log(convertInputToArr(inputData));
+const getFuelRequirementByMass = mass => mass.map(m => Math.floor(m / 3) - 2);
 
-const getFuelRequirementByMass = mass =>
-  mass.map(mass => Math.floor(mass / 3) - 2);
-
-console.log(getFuelRequirementByMass(convertInputToArr(inputData)));
-
-const getTotalFuel = fuelArr =>
+const reduceFuelArr = fuelArr =>
   fuelArr.reduce((total, currentValue) => total + currentValue);
 
-const test = R.compose(
-  getTotalFuel,
+const getTotalFuel = R.compose(
+  reduceFuelArr,
   getFuelRequirementByMass,
   convertInputToArr
 );
 
-console.log(test(inputData));
+//-------------------------------------------------------------------
+// PART TWO
+//-------------------------------------------------------------------
+const getFuelForFuelWeight = totalFuel => {
+  if (totalFuel <= 0) {
+    return totalFuel;
+  } else {
+    const newValue = getTotalFuel(totalFuel);
+    console.log({ newValue });
+
+    return (totalFuel += getFuelForFuelWeight(newValue));
+  }
+};
