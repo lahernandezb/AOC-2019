9d844b3decddd24ac0b20a1f4c6eff7b7b1277e2
Luis Hernandez <luis@heysparkbox.com>
2020-02-13 14:42:38 -0500

    test: add testing new testing for day 04 part 1
    Add testing for utlitly generateArray.js and day04.test.js

--- test/day04.test.js
+++ test/day04.test.js
@@ -3,12 +3,16 @@ const possiblePasswords = require('../day04/index');
 const { generateArray } = require('../day04/generateArray');
 
 describe('Password generator', () => {
-  it('returns an array', () => {
-    const start = 372304;
-    const end = 847068;
-    const array = generateArray(start, end);
-    const result = possiblePasswords(array);
+  const start = 1;
+  const end = 10;
+  const array = generateArray(start, end);
 
-    expect(result).to.be.an('array');
+  it('returns array [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]', () => {
+    expect(array).to.include.members([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  });
+
+  it('returns empty array []', () => {
+    const result = possiblePasswords(array);
+    expect(result).to.be.an('array').that.is.empty;
   });
 });
