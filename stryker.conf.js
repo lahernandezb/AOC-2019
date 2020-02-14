982a44fdff93b001fc6ac75a289cc4111634b331
Luis Hernandez <luis@heysparkbox.com>
2020-02-14 16:00:15 -0500

    test: update stryker config
    Update stryker.conf.js to ignore the interfacing js files.

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,6 +1,11 @@
 module.exports = function(config) {
   config.set({
-    mutate: ['./day01/*.js', './day04/*.js'],
+    mutate: [
+      './day01/*.js',
+      './day04/*.js',
+      '!./day04/generator.js',
+      '!./day04/index.js'
+    ],
     mutator: 'javascript',
     packageManager: 'npm',
     reporters: ['clear-text', 'progress', 'dashboard'],
