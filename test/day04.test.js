ba8d42d67107b4217eba23a9d6d9968f681c52a7
Luis Hernandez <luis@heysparkbox.com>
2020-02-14 16:06:47 -0500

    test: Update testing for passwordFinders
    Increase range and test for values of resulting array instead of testing for an empty array.

--- test/day04.test.js
+++ test/day04.test.js
@@ -1,18 +1,52 @@
 const { expect } = require('chai');
-const passswordFinder = require('../day04/passwordFinder1');
+const { passwordFinder1 } = require('../day04/passwordFinder1');
+const { passwordFinder2 } = require('../day04/passwordFinder2');
 const { generateArray } = require('../day04/generateArray');
 
 describe('Password generator', () => {
-  const start = 1;
+  const start = 5;
   const end = 10;
   const array = generateArray(start, end);
 
-  it('returns array [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]', () => {
-    expect(array).to.include.members([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+  it('returns array  5, 6, 7, 8, 9, 10]', () => {
+    expect(array)
+      .to.have.length(6)
+      .to.include.members([5, 6, 7, 8, 9, 10]);
   });
 
-  it('returns empty array []', () => {
-    const result = passswordFinder(array);
-    expect(result).to.be.an('array').that.is.empty;
+  it('returns array with 10 possible passwords (filter) ', () => {
+    const result = passwordFinder1(123456, 123567);
+    expect(result)
+      .to.have.length(10)
+      .and.to.include.members([
+        '123466',
+        '123477',
+        '123488',
+        '123499',
+        '123555',
+        '123556',
+        '123557',
+        '123558',
+        '123559',
+        '123566'
+      ]);
+  });
+
+  it('returns array with 10 possible passwords (loop) ', () => {
+    const result = passwordFinder2(123456, 123567);
+    expect(result)
+      .to.have.length(10)
+      .and.to.include.members([
+        '123466',
+        '123477',
+        '123488',
+        '123499',
+        '123555',
+        '123556',
+        '123557',
+        '123558',
+        '123559',
+        '123566'
+      ]);
   });
 });
