e16526928ff08c953b1a80abd4a3157ec5a51d39
Luis Hernandez <luis@heysparkbox.com>
2020-02-17 15:45:00 -0500

    feat: add imperative loop solution to day04 part 2
    Implemenation is similar to part2-filter.js with the exception of utilizing a for loop instead of a data pipe.

---
+++ day04/part2-loop.js
@@ -0,0 +1,27 @@
+const sortByNumericValue = (a, b) => a - b;
+
+const conditionCheck = numString =>
+  numString.length === 6 &&
+  numString === [...numString].sort(sortByNumericValue).join('');
+
+const passwordFinder2 = (start, end) => {
+  const result = [];
+
+  for (let i = start; i <= end; i++) {
+    const numString = i.toString();
+
+    if (conditionCheck(numString)) {
+      let cache = new Set();
+
+      [...numString].forEach(item => {
+        cache[item] = cache[item] ? cache[item] + 1 : 1;
+      });
+
+      Object.keys(cache).filter(key => cache[key] === 2).length !== 0 &&
+        result.push(numString);
+    }
+  }
+  return result;
+};
+
+console.log(passwordFinder2(372304, 847060).length);
