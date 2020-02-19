8e5bba134ce6dc4d28886d5261227010ae2f1380
Luis Hernandez <luis@heysparkbox.com>
2020-02-19 10:16:54 -0500

    test: add hmtl reports to stryker config

--- stryker.conf.js
+++ stryker.conf.js
@@ -4,11 +4,12 @@ module.exports = function(config) {
       './day01/*.js',
       './day04/*.js',
       '!./day04/generator.js',
-      '!./day04/index.js'
+      '!./day04/index.js',
+      '!./day04/passwordFinder3.js'
     ],
     mutator: 'javascript',
     packageManager: 'npm',
-    reporters: ['clear-text', 'progress', 'dashboard'],
+    reporters: ['clear-text', 'progress', 'html'],
     testRunner: 'mocha',
     transpilers: [],
     testFramework: 'mocha',
