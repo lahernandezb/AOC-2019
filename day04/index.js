5e7e6379dae31c83860ab527d362f074a07e18fe
Luis Hernandez <luis@heysparkbox.com>
2020-02-19 07:58:06 -0500

    style: remove comments on day04/index.js

--- day04/index.js
+++ day04/index.js
@@ -4,7 +4,3 @@ const getPasswordFilterPt2 = require('./doublePasswordFinder1');
 const getPasswordLoopPt2 = require('./doublePasswordFinder2');
 
 // Retrieve acceptable password options by calling one of methods with a start and end value.
-
-// getPasswordsFilter filters through a generated array of all possibilites within range.
-
-// getPasswordsLoop leverages a for loop to generarte the array of acceptable passwords.
